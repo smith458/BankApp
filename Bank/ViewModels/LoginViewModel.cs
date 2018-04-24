39a2afa4ca8168a21edbc72b02439c213e56e27d
Steven Smith <sbsmith@beckman.com>
2018-04-24 11:03:51 -0400

    WPF app with login screen and submit button that clears entries

---
+++ Bank/ViewModels/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MVVM;
+
+namespace ViewModels
+{
+    public class LoginViewModel : ObservableObject
+    {
+      private string _userName = "";
+      private string _password = "";
+      
+      public string UserName
+      {
+        get => _userName;
+        set
+        {
+          _userName = value;
+          RaisePropertyChangedEvent(nameof(UserName));
+        }
+      }
+
+      public string Password
+      {
+        get => _password;
+        set
+        {
+          _password = value;
+          RaisePropertyChangedEvent(nameof(Password));
+        }
+      }
+
+      public ICommand LoginCommand => new DelegateCommand(Login);
+
+      public void Login()
+      {
+        UserName = "";
+        Password = "";
+      }
+    }
+}
